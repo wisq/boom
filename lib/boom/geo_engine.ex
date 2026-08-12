0fdc0b2b3f67486285008e6c03d9c147b2b408df
Adrian Irving-Beer <wisq@wisq.net>
2026-08-12 14:20:36 -0400

    Add min_bounding_circle, will need it later.

--- lib/boom/geo_engine.ex
+++ lib/boom/geo_engine.ex
@@ -35,6 +35,27 @@ defmodule Boom.GeoEngine do
     |> Repo.one()
   end
 
+  @bounding_sql """
+  SELECT
+      ST_Centroid(circle) AS aim_point,
+      ST_Distance(
+          ST_Centroid(circle),
+          ST_PointN(ST_ExteriorRing(circle), 1)
+      ) AS radius
+  FROM (
+      SELECT ST_MinimumBoundingCircle(?::geometry) AS circle
+  )
+  """
+
+  def min_bounding_circle(geom) do
+    from(
+      q in fragment(@bounding_sql, ^geom),
+      select: [fragment("radius"), fragment("aim_point")]
+    )
+    |> Repo.one()
+    |> then(fn [radius, aim_point] -> {radius, aim_point} end)
+  end
+
   def split_multipolygon(%Geo.MultiPolygon{coordinates: polys}) do
     polys
     |> Enum.map(&%Geo.Polygon{coordinates: &1})
