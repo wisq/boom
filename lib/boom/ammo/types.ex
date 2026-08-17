2573ca22a2cfbd04a3e26de9f24a1589eefab281
Adrian Irving-Beer <wisq@wisq.net>
2026-08-16 20:46:38 -0400

    Add DRIL ammo.  Suggest by default, since it's so cheap.

--- lib/boom/ammo/types.ex
+++ lib/boom/ammo/types.ex
@@ -2,9 +2,11 @@ defmodule Boom.Ammo.Types do
   alias Boom.Ammo
 
   @ammo_types [
+    %Ammo{name: "DRIL", blast_radius: 70, auto_suggest: true},
     %Ammo{name: "AP", blast_radius: 135, auto_suggest: true},
     %Ammo{name: "HE", blast_radius: 270, auto_suggest: true},
-    %Ammo{name: "HCHE", blast_radius: 630, auto_suggest: true}
+    %Ammo{name: "HCHE", blast_radius: 630, auto_suggest: true},
+    %Ammo{name: "STAR", blast_radius: 12740}
   ]
 
   @types_by_name Map.new(@ammo_types, &{&1.name, &1})
